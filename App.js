5dc4007a10e75ea3a79f8fce5281250a965b543e
Meg Hermes <68392405+MegHermes@users.noreply.github.com>
2024-02-01 22:45:32 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Weather from './components/Weather';
+import '.././src/css/Styles.css';
+
+class App extends Component {
+  render() {
+    return <Weather />;
+  }
+}
+
+export default App;
