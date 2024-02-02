d333314cb1c00afc69204b5acfc132352c0ef002
Meg Hermes <68392405+MegHermes@users.noreply.github.com>
2024-02-01 22:47:13 -0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <div className="bottom">Weather App</div>;
+}
+
+export default About;
---
+++ Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Spinner.css';
+
+function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="anim"></div>
+    </div>
+  );
+}
+
+export default Spinner;
